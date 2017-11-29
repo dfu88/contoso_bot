4e92ac6fbd869e9e0014d20c67e30c407248a8f4
Dylan Fu <dylan.fu@hotmail.co.nz>
2017-11-29 15:38:13 +1300

    Updated Custom Vision Prediction URL

--- CognitiveVision.js
+++ CognitiveVision.js
@@ -3,7 +3,7 @@ var request = require('request'); //node module for http post requests
 exports.getMessage = function (session){
 
     request.post({
-        url: 'https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/c8753691-6547-43ea-99c2-0add0b4a447f/url?iterationId=a89cfe57-5a7b-4784-82fb-42ab0d1ee744',
+        url: 'https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/c8753691-6547-43ea-99c2-0add0b4a447f/url?iterationId=04e7b59e-de14-4a0c-9e39-cf6c0e37b2f8',
         json: true,
         headers: {
             'Content-Type': 'application/json',
@@ -18,7 +18,7 @@ exports.getMessage = function (session){
 
 function validResponse(body){
     if (body && body.Predictions && body.Predictions[0].Tag){
-        return "Type of money " + body.Predictions[0].Tag
+        return "Type of Money: " + body.Predictions[0].Tag
     } else{
         console.log('Wasn\'t recognised, please try again!');
     }
